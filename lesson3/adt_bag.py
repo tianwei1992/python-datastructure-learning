946a8c1d11d101eab616b582aa455cd52b6b3af9
tianwei1992 <879983690@qq.com>
2018-09-18 19:52:22 +0800

    lesson3

---
+++ lesson3/adt_bag.py
@@ -0,0 +1,33 @@
+class Bag(object):
+	def __init__(self, maxsize=10):
+		self.maxsize = maxsize
+		self._items = list()
+	def add(self, item):
+		if len(self._items) >= self.maxsize:
+			raise Exception("Bag is full!")
+		self._items.append(item)
+	def remove(self, item):
+		self._items.remove(item)
+	def __len__(self):
+		return len(self._items)
+	def __iter__(self):
+		# return iter(self._items)
+		for item in self._items:
+			yield item
+
+def test_bag():
+	bag = Bag()
+	bag.add(1)
+	bag.add(2)
+	bag.add(3)
+
+	assert len(bag) == 3
+
+	bag.remove(2)
+	assert len(bag) == 2
+
+	for item in bag:
+		print(item)
+
+if __name__ == "__main__":
+	test_bag()
